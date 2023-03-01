323eb112d99dcac17be6cefbc65b2c1830dfa223
mcastre1 <miguelcastrejon@mail.weber.edu>
2023-02-28 21:32:13 -0700

    Separation of concers, models.

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ from logging import Formatter, FileHandler
 from flask_wtf import Form
 from flask_migrate import Migrate
 from forms import *
+from models import db, Venue, Artist
+
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -20,66 +22,17 @@ from forms import *
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
+db.init_app(app)
 
-# TODO: connect to a local postgresql database
+# Connects to a local database using config file.
 migrate = Migrate(app, db)
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
 
-shows = db.Table('shows',
-                 db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True),
-                 db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True),
-                 db.Column('start_time', db.String)
-                 )
-
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    website = db.Column(db.String(500))
-    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
-    seeking_description = db.Column(db.String(500))
-    past_shows = db.Column(db.ARRAY(db.Integer))
-    past_shows_count = db.Column(db.Integer)
-    upcoming_shows = db.Column(db.ARRAY(db.Integer))
-    upcoming_shows_count = db.Column(db.Integer)
-    artists = db.relationship('Artist', secondary=shows, backref=db.backref('venue', lazy=True))
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String), nullable=False)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(500))
-    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
-    seeking_description = db.Column(db.String(500))
-    past_shows = db.Column(db.ARRAY(db.Integer))
-    past_shows_count = db.Column(db.Integer)
-    upcoming_shows = db.Column(db.ARRAY(db.Integer))
-    upcoming_shows_count = db.Column(db.Integer)
-
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+# Moved models to models.py. Separation of concerns.
 
   
 
---
+++ models.py
@@ -0,0 +1,50 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+shows = db.Table('shows',
+                 db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True),
+                 db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True),
+                 db.Column('start_time', db.String)
+                 )
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    website = db.Column(db.String(500))
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(500))
+    past_shows = db.Column(db.ARRAY(db.Integer))
+    past_shows_count = db.Column(db.Integer)
+    upcoming_shows = db.Column(db.ARRAY(db.Integer))
+    upcoming_shows_count = db.Column(db.Integer)
+    artists = db.relationship('Artist', secondary=shows, backref=db.backref('venue', lazy=True))
+
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String), nullable=False)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(500))
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(500))
+    past_shows = db.Column(db.ARRAY(db.Integer))
+    past_shows_count = db.Column(db.Integer)
+    upcoming_shows = db.Column(db.ARRAY(db.Integer))
+    upcoming_shows_count = db.Column(db.Integer)
